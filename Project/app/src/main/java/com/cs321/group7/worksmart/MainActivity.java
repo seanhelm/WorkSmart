65e5b91cb791f598cba98dd274b1e7d8a1242759
Chris Bradel <ChrisBradel14@gmail.com>
2017-10-16 02:34:29 -0400

    Hide some currently unused things

--- Project/app/src/main/java/com/cs321/group7/worksmart/MainActivity.java
+++ Project/app/src/main/java/com/cs321/group7/worksmart/MainActivity.java
@@ -88,17 +88,18 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_second) {
             Intent intent = new Intent(this, SecondActivity.class);
             startActivity(intent);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
+// else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
