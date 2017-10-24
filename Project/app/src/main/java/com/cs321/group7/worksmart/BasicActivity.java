95d123228370200dda4a8633be49838597504ae9
Chris Bradel <ChrisBradel14@gmail.com>
2017-10-24 16:51:14 -0400

    A bit more generalized

---
+++ Project/app/src/main/java/com/cs321/group7/worksmart/BasicActivity.java
@@ -0,0 +1,83 @@
+package com.cs321.group7.worksmart;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class BasicActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+    // Activity code here
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Intent intent;
+
+        switch(id)
+        {
+            case R.id.nav_homepage:
+                intent = new Intent(this, MainActivity.class);
+                break;
+            case R.id.nav_classlist:
+                intent = new Intent(this, ClassListActivity.class);
+                break;
+            default:
+                intent = new Intent(this, ClassListActivity.class); // Fallback
+        }
+
+        startActivity(intent);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
--- Project/app/src/main/java/com/cs321/group7/worksmart/ClassListActivity.java
+++ Project/app/src/main/java/com/cs321/group7/worksmart/ClassListActivity.java
@@ -1,14 +1,26 @@
 package com.cs321.group7.worksmart;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class ClassListActivity extends AppCompatActivity {
+public class ClassListActivity extends BasicActivity {
+    public static final String EXTRA_MESSAGE = "com.cs321.group7.worksmart.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_classlist);
+        setContentView(R.layout.content_classlist);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
 
