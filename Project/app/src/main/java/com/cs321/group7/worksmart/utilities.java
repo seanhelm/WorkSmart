06674f7a25d53aa2cb79fc0e15f3f4dd44e8f0cf
Lord Mendoza <lord.d.mendoza@gmail.com>
2017-11-14 14:06:23 -0500

    Revert "Added Methods"
    This reverts commit 9d99a76abd897407519fa1eaff66ea1154a81bf7.

--- Project/app/src/main/java/com/cs321/group7/worksmart/utilities.java
+++ Project/app/src/main/java/com/cs321/group7/worksmart/utilities.java
@@ -2,35 +2,5 @@ package com.cs321.group7.worksmart;
 
 public class utilities
 {
-    //Grade Related
-    getCurrentGrade(className){}
-    calculateGrade(className){}
-    addGrade(className, gradeName, score, classification){}
-    removeGrade(className, gradeName){}
-    addWeight(className, classificationName, weight){}
-    removeWeight(className, classificationName){}
-
-    //Task Related
-    getTaskList(className){}
-    addTask(className, taskName, dueDate, dueTime, priorityLevel, notes){}
-    removeTask(className, taskName){}
-    setReminder(className, taskName, taskName, remindValue, timeFormat){}
-
-    //Class Related
-    getClasses(semesterName){}
-    addClass(semesterName, className, facultyInfoOne, facultyInfoTwo, classLocation, classStart, classEnd, daysOfWeek){}
-    removeClass(semesterName, className){}
-
-    //Access Related
-    getEmailInfo(className, facultyIndex){}
-    toggleTextSize(fontSize){}
-    toggleColorAdjustment(colorIndex){}
-    toggleHighContrast(){}
-    closeApplication(){}
-
-    //Options Related
-    addProfileName(userName){}
-    setTimeFormat(timeSettingIndex){}
-    setDateFormat(dateSettingIndex){}
-
+    
 }
