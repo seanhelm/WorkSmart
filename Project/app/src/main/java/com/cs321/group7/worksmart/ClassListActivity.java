57a6735e7146c00a67349c6063df3b12938489d2
Chris Bradel <ChrisBradel14@gmail.com>
2017-11-26 22:33:05 -0500

    The "It Actually Works" Update
    I've basically started over. Now it isn't a horrible mess of broken
    things! This new baseline is what we'll use going forward.

--- Project/app/src/main/java/com/cs321/group7/worksmart/ClassListActivity.java
+++ Project/app/src/main/java/com/cs321/group7/worksmart/ClassListActivity.java
@@ -1,28 +1,27 @@
 package com.cs321.group7.worksmart;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
+/**
+ * Created by Chris on 11/26/2017.
+ */
 
 public class ClassListActivity extends BasicActivity {
-    public static final String EXTRA_MESSAGE = "com.cs321.group7.worksmart.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_classlist);
+        setContentView(R.layout.activity_classlist);
+    }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_classlist:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
-
-// For adding the drawer to everywhere
-// https://stackoverflow.com/questions/19451715/same-navigation-drawer-in-different-activities
