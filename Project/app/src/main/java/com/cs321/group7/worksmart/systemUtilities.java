089451b61d20d37e374d928ecaeb812fc1f26099
Chris Bradel <ChrisBradel14@gmail.com>
2017-11-26 23:41:26 -0500

    Add dummy return and fix git nag.
    The final master direct-commit!

--- Project/app/src/main/java/com/cs321/group7/worksmart/systemUtilities.java
+++ Project/app/src/main/java/com/cs321/group7/worksmart/systemUtilities.java
@@ -8,7 +8,7 @@ import com.cs321.group7.worksmart.Entities.*;
  * Created by Lord Mendoza on 11/15/2017.
  */
 
-public class systemUtilities
+public class SystemUtilities
 {
         //------------------------------------------------------------------------------------------
         //---------------------------Semester  Related----------------------------------------------
@@ -26,7 +26,7 @@ public class systemUtilities
         //Retrieves class from database using className, and returns the current grade
         public double getCurrentGrade(String className)
         {
-            
+            return -1;
         }
 
         //Updates the currentGrade variable of specified class
