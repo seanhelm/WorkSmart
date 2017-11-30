3703c76eeca8a42e1110bcd1b995dc3d622f1bac
Chris Bradel <ChrisBradel14@gmail.com>
2017-11-30 14:28:51 -0500

    Worj Support - Touch About Screen Things

--- Project/app/src/main/java/com/cs321/group7/worksmart/About.java
+++ Project/app/src/main/java/com/cs321/group7/worksmart/About.java
@@ -2,6 +2,8 @@ package com.cs321.group7.worksmart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity
 {
@@ -10,5 +12,22 @@ public class About extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        final TextView title = (TextView) findViewById(R.id.textView5);
+        final TextView title2 = (TextView) findViewById(R.id.textView11);
+
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                title.setText("WorjSmart - A Digital Student Agenda");
+            }
+        });
+
+        title2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                title2.setText("FLORJIN BLORJIN SMORJ");
+            }
+        });
     }
 }
